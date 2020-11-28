64d714497bf4130ddee3042a83cab657380897bb
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-11-28 16:52:40 -0300

    Rename Datasets & Add Exemplo3

--- Lab-Tasinaffo/ExemploAula3.m
+++ Lab-Tasinaffo/ExemploAula3.m
@@ -15,11 +15,21 @@ clc
 
 
 %% 1) Padrões de Entrada e Saída
-P = [ -1 -1  2  2;  0  5  0  5 ]
-T = [ -1 -1  1  1];
+camargos = load('datasets/01-camargos.txt');  % Vetor 42 x 17 (1971 - 2012)
+
+Pcamargos = [];
+Tcamargos = [];
+
+for i = 1:1:40
+    Pcamargos = [Pcamargos camargos(i,:)'];
+    Tcamargos = [Tcamargos camargos(i+1,:)'];
+end
+
+P = [Pcamargos];
+T = [Tcamargos];
 
 %% 2) Construção da Rede MLP
-net = feedforwardnet(3);
+net = feedforwardnet(30);
 net = configure(net, P, T);
 
 %% 3) Dividir Padrões
@@ -40,7 +50,7 @@ net.layers{2}.transferFcn = 'purelin';
 net.performFcn            = 'mse';
 net.trainFcn              = 'trainlm';
 net.trainParam.epochs     = 10000;
-net.trainParam.time       = 120;
+net.trainParam.time       = 240;
 net.trainParam.lr         = 0.2;
 net.trainParam.min_grad   = 10^-8;
 net.trainParam.max_fail   = 1000;
@@ -49,4 +59,33 @@ net.trainParam.max_fail   = 1000;
 
 %% 6) Simular as respostas de saída da rede MLP.
 
-a = sim(net, P);
+% 6.1 - Padrões de treinamento
+xP = 1:1:(41*12);
+xF = (41*12)+1:1:42*12;
+
+XcamargosP = [];
+for i = 1:1:41
+    XcamargosP = [XcamargosP camargos(i,:)];
+end
+XcamargosF = camargos(42,:);
+plot(xP, XcamargosP, 'b', xF, XcamargosF, 'r');
+xlabel('Meses');
+ylabel('Vazão');
+title('Vazão do Rio Camargo');
+grid
+
+% 6.2 - Resultados da Simulação
+hold on
+xS = 1:1:(42*12);
+PsA = camargos(1,:)';
+Ms = PsA;
+for i = 1:1:41
+    PsD = sim(net, PsA);
+    Ms = [Ms PsD];
+    PsA = PsD;
+end
+yS = [];
+for i = 1:1:42
+    yS = [yS Ms(:,i)'];
+end
+plot(xS, yS, ':m');
